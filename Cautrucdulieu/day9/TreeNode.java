f1f2c7ca2ca283920f340b5b70e855186a6a5eb9
Vietanh1311 <83020392+Vietanh1311@users.noreply.github.com>
2021-08-30 21:05:50 +0700

    Add files via upload

---
+++ Cautrucdulieu/day9/TreeNode.java
@@ -0,0 +1,9 @@
+public class TreeNode {
+    public int value; //Lưu giá trị node hiện tại
+    public TreeNode left;
+    public TreeNode right;
+    
+    public TreeNode(int value) {
+        this.value = value;
+    }
+}
