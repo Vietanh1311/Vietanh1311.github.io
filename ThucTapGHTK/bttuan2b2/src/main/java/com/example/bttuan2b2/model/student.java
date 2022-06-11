5d35c2cf639316b28a3f85935d6104376cb02fce
Vietanh1311 <83020392+Vietanh1311@users.noreply.github.com>
2022-06-11 22:36:58 +0700

    Add files via upload

---
+++ ThucTapGHTK/bttuan2b2/src/main/java/com/example/bttuan2b2/model/student.java
@@ -0,0 +1,34 @@
+package com.example.bttuan2b2.model;
+
+import java.util.Date;
+
+public class student {
+    public String mahocsinh;
+
+    public String stt;
+
+    public String ten;
+
+    public String truonghoc;
+
+    public String quanhuyen;
+
+    public String lop;
+
+    public String ngaysinh;
+
+    public String gioitinh;
+
+    public String noisinh;
+
+    public String dantoc;
+
+    public String hokhau;
+
+    public String dienthoai;
+
+    public String diem;
+
+    public String ghichu;
+
+}
---
+++ ThucTapGHTK/bttuan2b2/src/test/java/com/example/bttuan2b2/Bttuan2b2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bttuan2b2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Bttuan2b2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
