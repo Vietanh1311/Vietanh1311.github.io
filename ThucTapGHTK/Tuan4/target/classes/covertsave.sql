701c6fc718b2b562675a3c04f74211854027a3cd
Vietanh1311 <83020392+Vietanh1311@users.noreply.github.com>
2022-07-02 11:26:26 +0700

    Delete ThucTapGHTK/Tuan4 directory

--- ThucTapGHTK/Tuan4/target/classes/covertsave.sql
+++
@@ -1,4 +0,0 @@
-insert into category (id, name, code, description, status) values (1001, 'Chivas Regal - 19 Year Old', '8', 'Basil - Seedlings Cookstown', 0);
-
-REPLACE INTO product (id, description, name, price, sku, status, category_id)
-VALUES (1001,'Vinegar - Champagne3478','Scallops - In Shell3478',11410,'20011-014',0,1001);
